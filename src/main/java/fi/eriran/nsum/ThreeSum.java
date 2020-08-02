4d3f9e7ef1bbd42902eb34672d669ea1c2b39893
Erik <erik.rantanen@hotmail.fi>
2020-08-02 20:56:13 +0300

    Improve performance
    Array is sorted, some assumptions about the data are made to make
    process faster. Probably should get rid of the Set to make it pass the
    tests

--- src/main/java/fi/eriran/nsum/ThreeSum.java
+++ src/main/java/fi/eriran/nsum/ThreeSum.java
@@ -16,38 +16,35 @@ public class ThreeSum {
         if (nums.length < 3) {
             return new ArrayList<>();
         }
-        return findUniqueTriplets(nums);
+        return findUniqueTriplets(Arrays.copyOf(nums, nums.length));
     }
 
-    /**
-     * Use this array to store data about two pairs of numbers in the parameter array
-     */
     private List<List<Integer>> findUniqueTriplets(int[] nums) {
+        Arrays.sort(nums);
         Set<Triplet> foundTriplets = new TreeSet<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-                        Triplet potentialTriplet = new Triplet(nums[i], nums[j], nums[k]);
-                        if (!isPotentialAlreadyIncluded(potentialTriplet, foundTriplets)) {
-                            foundTriplets.add(potentialTriplet);
-                        }
-                    }
+            twoSum(nums, nums[i], i + 1, foundTriplets);
+        }
+        return convertToListResponse(foundTriplets);
+    }
+
+    public void twoSum(int[] nums, int target, int startPoint, Set<Triplet> foundTriplets) {
+        for (int i = startPoint; i < nums.length - 1; i++) {
+            for (int j = nums.length - 1; j > startPoint && j > i; j--) {
+                if (nums[i] + nums[j] + target == 0) {
+                    //foundTriplets is a set to only unique triplets are added
+                    Triplet triplet = new Triplet(target, nums[i], nums[j]);
+                    foundTriplets.add(triplet);
+                    break;
                 }
             }
         }
-        return convertToListResponse(foundTriplets);
     }
 
     private List<List<Integer>> convertToListResponse(Set<Triplet> foundTriplets) {
         return foundTriplets.stream()
                 .map(triplet -> Arrays.asList(triplet.one, triplet.two, triplet.three))
                 .collect(Collectors.toList());
-    }
-
-    private boolean isPotentialAlreadyIncluded(Triplet potentialTriplet,
-                                               Set<Triplet> response) {
-        return response.contains(potentialTriplet);
     }
 
     static class Triplet implements Comparable<Triplet> {
@@ -66,11 +63,11 @@ public class ThreeSum {
         @Override
         public int compareTo(Triplet o) {
             int oneComparison = Integer.compare(this.one, o.one);
-            if (oneComparison != 0){
+            if (oneComparison != 0) {
                 return oneComparison;
             }
             int twoComparison = Integer.compare(this.two, o.two);
-            if (twoComparison != 0){
+            if (twoComparison != 0) {
                 return twoComparison;
             }
             return Integer.compare(this.three, this.three);
--- src/test/java/fi/eriran/nsum/ThreeSumTest.java
+++ src/test/java/fi/eriran/nsum/ThreeSumTest.java
@@ -22,6 +22,20 @@ public class ThreeSumTest {
     }
 
     @Test
+    public void testExample2() {
+        List<List<Integer>> response = new ThreeSum().threeSum(new int[]{-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6});
+        assertFalse(CollectionUtils.isEmpty(response));
+        assertEquals(6, response.size());
+        assertTrue(response.contains(Arrays.asList(-4, -2, 6)));
+        assertTrue(response.contains(Arrays.asList(-4, 0, 4)));
+        assertTrue(response.contains(Arrays.asList(-4, 1, 3)));
+        assertTrue(response.contains(Arrays.asList(-4, 2, 2)));
+        assertTrue(response.contains(Arrays.asList(-2, -2, 4)));
+        assertTrue(response.contains(Arrays.asList(-2, 0, 2)));
+
+    }
+
+    @Test
     public void testLargeInput() {
         assertTimeout(ofMillis(50), () -> new ThreeSum().threeSum(new int[]{
                 -7, -10, -1, 3, 0, -7, -9, -1, 10, 8, -6, 4, 14, -8, 9, -15, 0, -4, -5, 9, 11, 3, -5, -8, 2, -6, -14, 7, -14, 10, 5, -6, 7, 11, 4, -7, 11
