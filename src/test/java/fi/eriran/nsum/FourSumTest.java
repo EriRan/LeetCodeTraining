59735ed899ea796654f44ea50d48e57720d95221
Erik <erik.rantanen@hotmail.fi>
2020-08-08 14:11:43 +0300

    Add testZeroesInput

--- src/test/java/fi/eriran/nsum/FourSumTest.java
+++ src/test/java/fi/eriran/nsum/FourSumTest.java
@@ -11,12 +11,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class FourSumTest {
 
     @Test
-    public void testExample() {
+    void testExample() {
         List<List<Integer>> lists = new FourSum().fourSum(new int[]{1, 0, -1, 0, -2, 2}, 0);
         assertFalse(CollectionUtils.isEmpty(lists));
         assertEquals(3, lists.size());
         assertTrue(lists.contains(Arrays.asList(-1, 0, 0, 1)));
         assertTrue(lists.contains(Arrays.asList(-2, -1, 1, 2)));
         assertTrue(lists.contains(Arrays.asList(-2, 0, 0, 2)));
+    }
+
+    @Test
+    void testZeroesInput() {
+        List<List<Integer>> lists = new FourSum().fourSum(new int[]{0,0,0,0}, 0);
+        assertFalse(CollectionUtils.isEmpty(lists));
+        assertEquals(1, lists.size());
+        assertTrue(lists.contains(Arrays.asList(0, 0, 0, 0)));
     }
 }
