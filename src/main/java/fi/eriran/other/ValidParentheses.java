381a2895e99e3304fa3c5deb9559583b374bd2ac
Erik <erik.rantanen@hotmail.fi>
2020-08-11 20:01:22 +0300

    Final cleanup

--- src/main/java/fi/eriran/other/ValidParentheses.java
+++ src/main/java/fi/eriran/other/ValidParentheses.java
@@ -3,6 +3,15 @@ package fi.eriran.other;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/**
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * <p>
+ * An input string is valid if:
+ * <p>
+ * 1. Open brackets must be closed by the same type of brackets.
+ * 2. Open brackets must be closed in the correct order.
+ * Note that an empty string is also considered valid.
+ */
 public class ValidParentheses {
 
     private static final char OPEN_PARENTHESIS = '(';
@@ -10,70 +19,62 @@ public class ValidParentheses {
     private static final char OPEN_SQUARE_BRACKET = '[';
     private static final char CLOSE_SQUARE_BRACKET = ']';
     private static final char OPEN_CURLY_BRACE = '{';
-    private static final char CLOSE_CURLY_BRACE  = '}';
+    private static final char CLOSE_CURLY_BRACE = '}';
 
-    public boolean isValid(String s) {
-        if (s == null) {
+    public boolean isValid(String string) {
+        if (string == null) {
             return false;
-        }
-        else if ("".equals(s)) {
+        } else if (string.isEmpty()) {
             return true;
         }
-        validateHasOnlyValidCharacters(s);
-        return iterateString(s);
+        validateHasOnlyValidCharacters(string);
+        return hasCorrectBrackets(string);
     }
 
-    private void validateHasOnlyValidCharacters(String s) {
-        for (char c : s.toCharArray()) {
-            if (!isValidCharacter(c)) {
-                throw new IllegalArgumentException("Invalid character encountered: " + c);
+    private void validateHasOnlyValidCharacters(String string) {
+        for (char character : string.toCharArray()) {
+            if (!isValidCharacter(character)) {
+                throw new IllegalArgumentException("Invalid character encountered: " + character);
             }
         }
     }
 
-    private boolean iterateString(String s) {
-        Deque<Character> deque = new ArrayDeque<>();
-        for (char currentCharacter : s.toCharArray()) {
-            if (deque.isEmpty()) {
-                if (isCloseBracket(currentCharacter)) {
-                    return false;
-                }
-                deque.add(currentCharacter);
+    private boolean hasCorrectBrackets(String string) {
+        Deque<Character> openBracketsDeque = new ArrayDeque<>();
+        for (char currentCharacter : string.toCharArray()) {
+            if (isCharacterValid(openBracketsDeque, currentCharacter)) return false;
+        }
+        return openBracketsDeque.isEmpty();
+    }
+
+    private boolean isCharacterValid(Deque<Character> openBracketsDeque, char currentCharacter) {
+        if (openBracketsDeque.isEmpty()) {
+            if (isCloseBracket(currentCharacter)) {
+                return true;
+            }
+            openBracketsDeque.push(currentCharacter);
+        } else {
+            if (isOpenBracket(currentCharacter)) {
+                openBracketsDeque.push(currentCharacter);
             } else {
-                if (isOpenBracket(currentCharacter)) {
-                    deque.push(currentCharacter);
-                } else {
-                    Character topCharacter = deque.pop();
-                    if (!isCorrectBracketClosed(topCharacter, currentCharacter)) {
-                        return false;
-                    }
-                }
+                Character topCharacter = openBracketsDeque.pop();
+                return !isCorrectBracketClosed(topCharacter, currentCharacter);
             }
         }
-        return deque.isEmpty();
+        return false;
     }
 
     private boolean isCorrectBracketClosed(Character topCharacter, char currentCharacter) {
         switch (topCharacter) {
             case OPEN_PARENTHESIS:
-                if (currentCharacter == CLOSE_PARENTHESIS) {
-                    return true;
-                }
-                break;
+                return currentCharacter == CLOSE_PARENTHESIS;
             case OPEN_SQUARE_BRACKET:
-                if (currentCharacter == CLOSE_SQUARE_BRACKET) {
-                    return true;
-                }
-                break;
+                return currentCharacter == CLOSE_SQUARE_BRACKET;
             case OPEN_CURLY_BRACE:
-                if (currentCharacter == CLOSE_CURLY_BRACE) {
-                    return true;
-                }
-                break;
+                return currentCharacter == CLOSE_CURLY_BRACE;
             default:
                 return false;
         }
-        return false;
     }
 
     private boolean isCloseBracket(char c) {
