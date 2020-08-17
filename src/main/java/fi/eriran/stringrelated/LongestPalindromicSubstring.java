0773ebc3478c262bbfe1fef479f214dfe645a317
Erik <erik.rantanen@hotmail.fi>
2020-08-17 19:42:12 +0300

    Add while loop based palindrome search

--- src/main/java/fi/eriran/stringrelated/LongestPalindromicSubstring.java
+++ src/main/java/fi/eriran/stringrelated/LongestPalindromicSubstring.java
@@ -5,16 +5,35 @@ package fi.eriran.stringrelated;
  */
 public class LongestPalindromicSubstring {
 
-    public String longestPalindrome(String s) {
-        if (s == null || s.isEmpty()) {
+    public String longestPalindrome(String string) {
+        if (string == null || string.isEmpty()) {
             return "";
         }
-        if (s.length() == 1) {
-            return s;
+        if (string.length() == 1) {
+            return string;
         }
-        if (s.length() > 1000) {
-            throw new IllegalArgumentException("Max String length is 1000");
+        if (string.length() > 1000) {
+            throw new IllegalArgumentException("Max allowed String length is 1000");
         }
-        return "";
+        return findLongest(string);
+    }
+
+    private String findLongest(String string) {
+        char[] charArray = string.toCharArray();
+
+        int leftPointer = 0;
+        int rightPointer = charArray.length - 1;
+        int palindromeStartIndex = leftPointer;
+        int palindromeEndIndex = rightPointer;
+
+        while (leftPointer <= rightPointer) {
+            if (charArray[leftPointer] != charArray[rightPointer]) {
+                palindromeStartIndex = leftPointer + 1;
+                palindromeEndIndex = rightPointer - 1;
+            }
+            leftPointer++;
+            rightPointer--;
+        }
+        return string.substring(palindromeStartIndex, palindromeEndIndex + 1);
     }
 }
