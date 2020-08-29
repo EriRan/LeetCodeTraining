348ebbb09905ec54a568ccc33e892651dbf27d48
Erik <erik.rantanen@hotmail.fi>
2020-08-29 13:36:02 +0300

    Add implementation that passes example test
    Suspicious about the previousSetLast hassle

--- src/main/java/fi/eriran/leetcode/linkedlist/SwapNodesInPairs.java
+++ src/main/java/fi/eriran/leetcode/linkedlist/SwapNodesInPairs.java
@@ -8,10 +8,48 @@ package fi.eriran.leetcode.linkedlist;
 public class SwapNodesInPairs {
 
     public ListNode swapPairs(ListNode head) {
+        validateInput(head);
+        //Shortcuts
+        if (head.next == null) {
+            return head;
+        }
+        return performSwapping(head);
+    }
+
+    private ListNode performSwapping(ListNode head) {
+        ListNode currentNode = head;
+        ListNode nextNode = currentNode.next;
+        ListNode previousSetLast = null;
+        head = nextNode;
+        while (nextNode != null) {
+            //Current node is made to point the next of the next
+            //Next node is made to point the current node
+            //Previous set last is made to point the next node
+            ListNode nextOfPreviousNext = nextNode.next;
+            nextNode.next = currentNode;
+            currentNode.next = nextOfPreviousNext;
+
+            if (previousSetLast != null) {
+                previousSetLast.next = nextNode;
+                previousSetLast = currentNode;
+            } else {
+                previousSetLast = currentNode;
+            }
+
+            currentNode = nextOfPreviousNext;
+            if (nextOfPreviousNext != null) {
+                nextNode = nextOfPreviousNext.next;
+            } else {
+                return head;
+            }
+        }
+        return head;
+    }
+
+    private void validateInput(ListNode head) {
         if (head == null) {
             throw new IllegalArgumentException("Null input not allowed");
         }
-        return null;
     }
 
     static class ListNode {
