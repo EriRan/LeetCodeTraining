0a76852c3e79ec97a00d5b3bb29e67d9a4105c67
Erik <erik.rantanen@hotmail.fi>
2020-08-30 18:28:34 +0300

    Change to two pointer solution
    The smallest max found edge is a guarantee of what is the maximum amount
    of water we can have on a single iteration. I can see some ways to trick
    this but let's submit this to LeetCode and get a test case that breaks
    this implementation

--- src/main/java/fi/eriran/leetcode/arrays/TrappingRainWater.java
+++ src/main/java/fi/eriran/leetcode/arrays/TrappingRainWater.java
@@ -22,29 +22,59 @@ public class TrappingRainWater {
 
     private int findTrapCount(int[] height) {
         int rainWaterCount = 0;
-        int potentialRainWater = 0;
 
-        for (int i = findFirstNonZero(height); i < height.length; i++) {
-            int currentHeight = height[i];
-            //Find "the next side of the pond"
-            for (int j = i + 1; j < height.length; j++) {
-                int pondHeigth = height[j];
-                if (pondHeigth < currentHeight) {
-                    potentialRainWater += currentHeight - pondHeigth;
-                } else {
-                    rainWaterCount += potentialRainWater;
-                    i = j - 1;
-                    break;
-                }
-            }
-            potentialRainWater = 0;
+        int leftPointer = findFirstNonZeroFromLeft(height);
+        int rightPointer = findFirstNonZeroFromRight(height);
+        int leftFoundMax = 0;
+        int rightFoundMax = 0;
+
+        while (leftPointer < rightPointer) {
+            int leftHeight = height[leftPointer];
+            int rightHeight = height[rightPointer];
+
+            leftFoundMax = tryToChangeMax(leftFoundMax, leftHeight);
+            rightFoundMax = tryToChangeMax(rightFoundMax, rightHeight);
+
+            int smallerMax = Math.min(leftFoundMax, rightFoundMax);
+
+            rainWaterCount += getWaterAmount(leftHeight, smallerMax);
+            rainWaterCount += getWaterAmount(rightHeight, smallerMax);
+
+            leftPointer++;
+            rightPointer--;
+        }
+        if (leftPointer == rightPointer) {
+            int leftHeight = height[leftPointer];
+            leftFoundMax = tryToChangeMax(leftFoundMax, leftHeight);
+            int smallerMax = Math.min(leftFoundMax, rightFoundMax);
+            rainWaterCount += getWaterAmount(leftHeight, smallerMax);
         }
 
         //Some kind of two pointer solution?
         return rainWaterCount;
     }
 
-    private int findFirstNonZero(int[] height) {
+    private int getWaterAmount(int height, int maximumPossible) {
+        return Math.max(maximumPossible - height, 0);
+    }
+
+    private int tryToChangeMax(int leftFoundMax, int leftHeight) {
+        if (leftFoundMax < leftHeight) {
+            leftFoundMax = leftHeight;
+        }
+        return leftFoundMax;
+    }
+
+    private int findFirstNonZeroFromRight(int[] height) {
+        for (int i = height.length - 1; i > 0; i--) {
+            if (height[i] != 0) {
+                return i;
+            }
+        }
+        return height.length;
+    }
+
+    private int findFirstNonZeroFromLeft(int[] height) {
         for (int i = 0; i < height.length; i++) {
             if (height[i] != 0) {
                 return i;
