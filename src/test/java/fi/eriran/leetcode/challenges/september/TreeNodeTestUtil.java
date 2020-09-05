35a802ef71040d75aa456fc7fa0eb882dd72fcfc
Erik <erik.rantanen@hotmail.fi>
2020-09-05 16:02:42 +0300

    Add TreeNode and testing util for it
    Needed for a new LeetCode's September challenge

---
+++ src/test/java/fi/eriran/leetcode/challenges/september/TreeNodeTestUtil.java
@@ -0,0 +1,45 @@
+package fi.eriran.leetcode.challenges.september;
+
+public class TreeNodeTestUtil {
+
+    private TreeNodeTestUtil() {
+    }
+
+    public static TreeNode createBinarySearchTree(Integer[] ints) {
+        if (ints == null || ints.length == 0) {
+            return new TreeNode();
+        }
+        TreeNode root = new TreeNode(ints[0]);
+        for (int i = 1; i < ints.length; i++) {
+            Integer currentNumber = ints[i];
+            if (currentNumber == null) {
+                continue;
+            }
+            placeNode(root, currentNumber);
+        }
+        return root;
+    }
+
+    private static void placeNode(TreeNode currentNode, int number) {
+        switch (Integer.compare(currentNode.val, number)) {
+            case 1:
+                if (currentNode.right == null) {
+                    currentNode.right = new TreeNode(number);
+                } else {
+                    placeNode(currentNode.right, number);
+                }
+                break;
+            case -1:
+                if (currentNode.left == null) {
+                    currentNode.left = new TreeNode(number);
+                } else {
+                    placeNode(currentNode.left, number);
+                }
+                break;
+            case 0:
+                throw new IllegalArgumentException("Duplicates now allowed");
+            default:
+                throw new IllegalStateException("Unknown result from number comparison");
+        }
+    }
+}
