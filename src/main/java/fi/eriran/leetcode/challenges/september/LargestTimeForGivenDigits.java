db5b67821649637e9e6c5f53cfeb034f540666f8
Erik <erik.rantanen@hotmail.fi>
2020-09-05 17:07:36 +0300

    Add skeleton for new algorithm

---
+++ src/main/java/fi/eriran/leetcode/challenges/september/LargestTimeForGivenDigits.java
@@ -0,0 +1,8 @@
+package fi.eriran.leetcode.challenges.september;
+
+public class LargestTimeForGivenDigits {
+
+    public String largestTimeFromDigits(int[] arr) {
+        return null;
+    }
+}
---
+++ src/test/java/fi/eriran/leetcode/challenges/september/LargestTimeForGivenDigitsTest.java
@@ -0,0 +1,29 @@
+package fi.eriran.leetcode.challenges.september;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LargestTimeForGivenDigitsTest {
+
+    @Test
+    void example1() {
+        assertEquals("23:41", new LargestTimeForGivenDigits().largestTimeFromDigits(new int[]{1, 2, 3, 4}));
+    }
+
+    @Test
+    void example2() {
+        assertTrue(StringUtils.isEmpty(new LargestTimeForGivenDigits().largestTimeFromDigits(new int[]{5, 5, 5, 5})));
+    }
+
+    @Test
+    void example3() {
+        assertEquals("00:00", new LargestTimeForGivenDigits().largestTimeFromDigits(new int[]{0, 0, 0, 0}));
+    }
+
+    @Test
+    void example4() {
+        assertEquals("10:00", new LargestTimeForGivenDigits().largestTimeFromDigits(new int[]{0, 0, 1, 0}));
+    }
+}
