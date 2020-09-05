55763023b1d8cf4f9f61185552fe2cbc2ba285d1
Erik <erik.rantanen@hotmail.fi>
2020-09-05 16:44:38 +0300

    Add implementation that passes all example tests

--- src/main/java/fi/eriran/leetcode/challenges/september/AllElementsInTwoBinarySearchTrees.java
+++ src/main/java/fi/eriran/leetcode/challenges/september/AllElementsInTwoBinarySearchTrees.java
@@ -1,6 +1,7 @@
 package fi.eriran.leetcode.challenges.september;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -8,6 +9,43 @@ import java.util.List;
  */
 public class AllElementsInTwoBinarySearchTrees {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        return new ArrayList<>();
+        if (root1 == null && root2 == null) {
+            return new ArrayList<>();
+        }
+        return returnInAscendingOrder(root1, root2);
+    }
+
+    private List<Integer> returnInAscendingOrder(TreeNode root1, TreeNode root2) {
+        return combineAndSort(
+                turnIntoList(root1),
+                turnIntoList(root2)
+        );
+    }
+
+    private List<Integer> combineAndSort(List<Integer> root1List, List<Integer> root2List) {
+        List<Integer> combinedArrays = new ArrayList<>();
+        combinedArrays.addAll(root1List);
+        combinedArrays.addAll(root2List);
+        Collections.sort(combinedArrays);
+        return combinedArrays;
+    }
+
+    private List<Integer> turnIntoList(TreeNode root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+        List<Integer> nodeList = new ArrayList<>();
+        findNextNumber(root, nodeList);
+        return nodeList;
+    }
+
+    private void findNextNumber(TreeNode root, List<Integer> nodeList) {
+        if (root.left != null) {
+            findNextNumber(root.left, nodeList);
+        }
+        nodeList.add(root.val);
+        if (root.right != null) {
+            findNextNumber(root.right, nodeList);
+        }
     }
 }
