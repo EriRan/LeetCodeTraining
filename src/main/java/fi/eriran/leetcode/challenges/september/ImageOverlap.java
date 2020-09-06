fd199bd9682b976d7965d0ecc146baec9e239fa7
Erik <erik.rantanen@hotmail.fi>
2020-09-06 18:26:31 +0300

    Add ImageOverlap skeleton

---
+++ src/main/java/fi/eriran/leetcode/challenges/september/ImageOverlap.java
@@ -0,0 +1,21 @@
+package fi.eriran.leetcode.challenges.september;
+
+/**
+ * Two images A and B are given, represented as binary, square matrices of the same size.
+ * (A binary matrix has only 0s and 1s as values.)
+ * <p>
+ * We translate one image however we choose (sliding it left, right, up, or down any number of units), and place it on
+ * top of the other image.  After, the overlap of this translation is the number of positions that have a 1 in both
+ * images.
+ * <p>
+ * (Note also that a translation does not include any kind of rotation.)
+ * <p>
+ * What is the largest possible overlap?
+ */
+public class ImageOverlap {
+
+    public int largestOverlap(int[][] A, int[][] B) {
+        return 0;
+    }
+
+}
---
+++ src/test/java/fi/eriran/leetcode/challenges/september/ImageOverlapTest.java
@@ -0,0 +1,25 @@
+package fi.eriran.leetcode.challenges.september;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageOverlapTest {
+
+    @Test
+    void example1() {
+        assertEquals(3, new ImageOverlap()
+                .largestOverlap(
+                        new int[][] {
+                                {1,1,0},
+                                {0,1,0},
+                                {0,1,0},
+                        },
+                        new int[][] {
+                                {0,0,0},
+                                {0,1,1},
+                                {0,0,1},
+                        }
+                ));
+    }
+}
