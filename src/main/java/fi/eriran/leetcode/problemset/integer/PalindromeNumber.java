642dc13781339e644a8ac9518d0bb8c1686bfb58
Erik <erik.rantanen@hotmail.fi>
2020-09-20 17:51:02 +0300

    Add Jdoc

--- src/main/java/fi/eriran/leetcode/problemset/integer/PalindromeNumber.java
+++ src/main/java/fi/eriran/leetcode/problemset/integer/PalindromeNumber.java
@@ -1,5 +1,12 @@
 package fi.eriran.leetcode.problemset.integer;
 
+/**
+ * Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+ *
+ * Follow up:
+ *
+ * Could you solve it without converting the integer to a string?
+ */
 public class PalindromeNumber {
 
     public boolean isPalindrome(int number) {
