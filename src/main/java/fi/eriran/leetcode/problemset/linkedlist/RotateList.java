f874edf9b19a5a35a38ccda9c05176c30e90a97d
Erik <erik.rantanen@hotmail.fi>
2021-02-22 17:22:55 +0200

    Add RotateList
    Passes example tests

---
+++ src/main/java/fi/eriran/leetcode/problemset/linkedlist/RotateList.java
@@ -0,0 +1,38 @@
+package fi.eriran.leetcode.problemset.linkedlist;
+
+public class RotateList {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (k == 0 || head == null || head.next == null) {
+            return head;
+        }
+        //Find first, tail and the one before tail
+        ListNode first = head;
+        ListNode previous = head;
+        ListNode tail = head;
+        boolean sizeCounted = false;
+        int size = 1;
+        int currentIterations = 0;
+        while (currentIterations < k) {
+            while (tail.next != null) {
+                previous = tail;
+                tail = tail.next;
+                if (!sizeCounted) {
+                    size++;
+                }
+            }
+            if (size != 0 && k % size == 0) {
+                return head;
+            }
+            //Move next pointers
+            previous.next = null;
+            tail.next = first;
+            //The previous last is now the first
+            first = tail;
+            sizeCounted = true;
+            currentIterations++;
+        }
+
+
+        return first;
+    }
+}
---
+++ src/test/java/fi/eriran/leetcode/problemset/linkedlist/RotateListTest.java
@@ -0,0 +1,89 @@
+package fi.eriran.leetcode.problemset.linkedlist;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RotateListTest {
+
+    @Test
+    void example1() {
+        ListNode currentNode = new RotateList()
+                .rotateRight(new ListNodeTestObjectGenerator().create(1, 2, 3, 4, 5), 2);
+        for (int i = 0; i < 5; i++) {
+            switch (i) {
+                case 0:
+                    assertEquals(4, currentNode.val);
+                    break;
+                case 1:
+                    assertEquals(5, currentNode.val);
+                    break;
+                case 2:
+                    assertEquals(1, currentNode.val);
+                    break;
+                case 3:
+                    assertEquals(2, currentNode.val);
+                    break;
+                case 4:
+                    assertEquals(3, currentNode.val);
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected index");
+            }
+            currentNode = currentNode.next;
+        }
+    }
+
+    @Test
+    void example2() {
+        ListNode currentNode = new RotateList()
+                .rotateRight(new ListNodeTestObjectGenerator().create(0,1,2), 4);
+        for (int i = 0; i < 3; i++) {
+            switch (i) {
+                case 0:
+                    assertEquals(2, currentNode.val);
+                    break;
+                case 1:
+                    assertEquals(0, currentNode.val);
+                    break;
+                case 2:
+                    assertEquals(1, currentNode.val);
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected index");
+            }
+            currentNode = currentNode.next;
+        }
+    }
+
+    /**
+     * If the amount of rotates can be divided with a modulo of zero, the rotation will not change the node at all.
+     */
+    @Test
+    void hugeNumberOfRotatesSkipped() {
+        ListNode currentNode = new RotateList()
+                .rotateRight(new ListNodeTestObjectGenerator().create(1, 2, 3, 4, 5), 2000000000);
+        for (int i = 0; i < 5; i++) {
+            switch (i) {
+                case 0:
+                    assertEquals(1, currentNode.val);
+                    break;
+                case 1:
+                    assertEquals(2, currentNode.val);
+                    break;
+                case 2:
+                    assertEquals(3, currentNode.val);
+                    break;
+                case 3:
+                    assertEquals(4, currentNode.val);
+                    break;
+                case 4:
+                    assertEquals(5, currentNode.val);
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected index");
+            }
+            currentNode = currentNode.next;
+        }
+    }
+}
