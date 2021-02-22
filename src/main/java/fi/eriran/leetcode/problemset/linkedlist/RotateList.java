157d4dfdf92da475184b831cf0bbc4479509413a
Erik <erik.rantanen@hotmail.fi>
2021-02-22 17:41:26 +0200

    Revert optimization
    The results are nearly identical and the previous implementation I used
    was more clear so let's go back to using it

--- src/main/java/fi/eriran/leetcode/problemset/linkedlist/RotateList.java
+++ src/main/java/fi/eriran/leetcode/problemset/linkedlist/RotateList.java
@@ -5,38 +5,40 @@ public class RotateList {
         if (k == 0 || head == null || head.next == null) {
             return head;
         }
+        k = deduceRealAmountOfRotationsNeeded(countSize(head), k);
         //Find first, tail and the one before tail
         ListNode first = head;
         ListNode previous = head;
         ListNode tail = head;
-        boolean sizeCounted = false;
-        int size = 1;
         int currentIterations = 0;
         while (currentIterations < k) {
             while (tail.next != null) {
                 previous = tail;
                 tail = tail.next;
-                if (!sizeCounted) {
-                    size++;
-                }
-            }
-            //Optimize the real count of rotations needed
-            if (!sizeCounted) {
-                k = k % size;
-                //If k is zero, we should stop rotating right away!
-                if (k == 0) {
-                    return head;
-                }
             }
             //Move next pointers
             previous.next = null;
             tail.next = first;
             //The previous last is now the first
             first = tail;
-            sizeCounted = true;
             currentIterations++;
         }
 
+
         return first;
+    }
+
+    private int deduceRealAmountOfRotationsNeeded(int countSize, int rotationsRequested) {
+        return rotationsRequested % countSize;
+    }
+
+    private int countSize(ListNode head) {
+        ListNode tail = head;
+        int size = 1;
+        while (tail.next != null) {
+            tail = tail.next;
+            size++;
+        }
+        return size;
     }
 }
