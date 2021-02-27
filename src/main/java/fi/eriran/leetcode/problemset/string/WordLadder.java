be121d9bb9b7876bc274cb5bc9eca4fed938fe44
Erik <erik.rantanen@hotmail.fi>
2021-02-27 15:11:46 +0200

    Add WordLadder skeleton

---
+++ src/main/java/fi/eriran/leetcode/problemset/string/WordLadder.java
@@ -0,0 +1,10 @@
+package fi.eriran.leetcode.problemset.string;
+
+import java.util.List;
+
+public class WordLadder {
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        return 0;
+    }
+}
---
+++ src/test/java/fi/eriran/leetcode/problemset/string/WordLadderTest.java
@@ -0,0 +1,36 @@
+package fi.eriran.leetcode.problemset.string;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordLadderTest {
+
+    @Test
+    void example1() {
+        assertEquals(
+                5,
+                new WordLadder()
+                        .ladderLength(
+                                "hit",
+                                "cog",
+                                Arrays.asList("hot","dot","dog","lot","log","cog")
+                        )
+                );
+    }
+
+    @Test
+    void example2() {
+        assertEquals(
+                5,
+                new WordLadder()
+                        .ladderLength(
+                                "hit",
+                                "cog",
+                                Arrays.asList("hot","dot","dog","lot","log")
+                        )
+        );
+    }
+}
