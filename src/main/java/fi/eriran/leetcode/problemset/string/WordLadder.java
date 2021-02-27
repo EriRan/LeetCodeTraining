0a49bbbcdb3b6465c66671926662bcd2f5d03efa
Erik <erik.rantanen@hotmail.fi>
2021-02-27 19:13:54 +0200

    Add breadth first search to get shortest path
    Passes the submission test with 733 ms and 40.2 MB memory used

--- src/main/java/fi/eriran/leetcode/problemset/string/WordLadder.java
+++ src/main/java/fi/eriran/leetcode/problemset/string/WordLadder.java
@@ -27,13 +27,14 @@ public class WordLadder {
         if (startNode.getConnections().isEmpty()) {
             return 0;
         }
-        return 0;
+        return findShortestPath(startNode, endWord);
     }
 
     private GraphNode buildGraph(String beginWord, List<String> wordList) {
         //Create start node
         GraphNode startNode = new GraphNode(beginWord);
         Map<String, GraphNode> allNodes = new HashMap<>();
+        allNodes.put(startNode.getValue(), startNode);
         findConnections(startNode, wordList, allNodes);
         return startNode;
     }
@@ -50,12 +51,44 @@ public class WordLadder {
                     findConnections(newNode, wordList, allNodes);
                 } else {
                     GraphNode existingNode = allNodes.get(word);
-                    allNodes.put(existingNode.getValue(), existingNode);
                     currentNode.getConnections().add(existingNode);
                 }
             }
         }
+    }
 
+    private int findShortestPath(GraphNode startNode, String endWord) {
+        Deque<GraphNode> deque = new ArrayDeque<>();
+        startNode.setVisited(true);
+        deque.add(startNode);
+        Map<String, String> pathMap = createPathMap(deque);
+        if (!pathMap.containsKey(endWord)) {
+            return 0;
+        }
+
+        //Let's calculate the path length
+        int shortestPathLength = 1;
+        String currentNodeValue = pathMap.get(endWord);
+        do {
+            shortestPathLength++;
+            currentNodeValue = pathMap.get(currentNodeValue);
+        } while (currentNodeValue != null);
+        return shortestPathLength;
+    }
+
+    private Map<String, String> createPathMap(Deque<GraphNode> deque) {
+        Map<String, String> pathMap = new HashMap<>();
+        while (!deque.isEmpty()) {
+            GraphNode currentNode = deque.removeFirst();
+            for (GraphNode neighbour : currentNode.getConnections()) {
+                if (!neighbour.isVisited()) {
+                    deque.addLast(neighbour);
+                    neighbour.setVisited(true);
+                    pathMap.put(neighbour.getValue(), currentNode.getValue());
+                }
+            }
+        }
+        return pathMap;
     }
 
     private boolean isOnlyOneCharacterDifferent(String wordOne, String wordTwo) {
@@ -83,7 +116,7 @@ public class WordLadder {
 
         private String value;
         private boolean visited;
-        private List<GraphNode> connections;
+        private final List<GraphNode> connections;
 
         public GraphNode() {
             visited = false;
@@ -106,6 +139,25 @@ public class WordLadder {
 
         public List<GraphNode> getConnections() {
             return connections;
+        }
+
+        public void setVisited(boolean visited) {
+            this.visited = visited;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            GraphNode graphNode = (GraphNode) o;
+
+            return value != null ? value.equals(graphNode.value) : graphNode.value == null;
+        }
+
+        @Override
+        public int hashCode() {
+            return value != null ? value.hashCode() : 0;
         }
     }
 }
